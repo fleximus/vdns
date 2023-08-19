e1c12d4f5a10dc583666976fa7947a5df860c310
Felix Ehlers <fleximus@users.noreply.github.com>
2023-08-19 02:10:20 +0200

    Tests: Helper checks, public resolver & cleanups

--- test/vdns_test.v
+++ test/vdns_test.v
@@ -1,39 +1,56 @@
 import vdns
 
-fn test_vdns() {
-	resolver := '192.168.178.1:53'
+fn test_str_to_type() {
+	assert(vdns.str_to_type('a') == vdns.Type.a)
+	assert(vdns.str_to_type('aaaa') == vdns.Type.aaaa)
+	assert(vdns.str_to_type('axfr') == vdns.Type.axfr)
+	assert(vdns.str_to_type('caa') == vdns.Type.caa)
+	assert(vdns.str_to_type('cname') == vdns.Type.cname)
+	assert(vdns.str_to_type('ixfr') == vdns.Type.ixfr)
+	assert(vdns.str_to_type('mx') == vdns.Type.mx)
+	assert(vdns.str_to_type('ns') == vdns.Type.ns)
+	assert(vdns.str_to_type('ptr') == vdns.Type.ptr)
+	assert(vdns.str_to_type('soa') == vdns.Type.soa)
+	assert(vdns.str_to_type('spf') == vdns.Type.spf)
+	assert(vdns.str_to_type('txt') == vdns.Type.txt)
+}
 
+fn test_type_to_str() {
+	assert(vdns.type_to_str(vdns.Type.a) == 'a')
+	assert(vdns.type_to_str(vdns.Type.aaaa) == 'aaaa')
+	assert(vdns.type_to_str(vdns.Type.axfr) == 'axfr')
+	assert(vdns.type_to_str(vdns.Type.caa) == 'caa')
+	assert(vdns.type_to_str(vdns.Type.cname) == 'cname')
+	assert(vdns.type_to_str(vdns.Type.ixfr) == 'ixfr')
+	assert(vdns.type_to_str(vdns.Type.mx) == 'mx')
+	assert(vdns.type_to_str(vdns.Type.ns) == 'ns')
+	assert(vdns.type_to_str(vdns.Type.ptr) == 'ptr')
+	assert(vdns.type_to_str(vdns.Type.soa) == 'soa')
+	assert(vdns.type_to_str(vdns.Type.spf) == 'spf')
+	assert(vdns.type_to_str(vdns.Type.txt) == 'txt')
+}
+
+fn test_vdns() {
+	resolver := '1.1.1.1:53'
 	mut result := vdns.Response{}
 
 	// Test A record
-	print('Testing A record...')
 	result = vdns.query(vdns.Query{ domain: 'fleximus.org', resolver: resolver }) !
 	assert(result.answers[0].record == '95.216.116.79')
-	println('OK!')
 
 	// Test AAAA record
-	print('Testing AAAA record...')
 	result = vdns.query(vdns.Query{ domain: 'fleximus.org', @type: vdns.Type.aaaa, resolver: resolver }) !
 	assert(result.answers[0].record == '2a01:04f9:002b:1a1c:5457:76dc:a4cf:0180')
-	println('OK!')
 
 	// Test CAA record
-	print('Testing CAA record...')
 	result = vdns.query(vdns.Query{ domain: 'fleximus.org', @type: vdns.Type.caa, resolver: resolver }) !
 	assert(result.answers[0].record == '0 issue letsencrypt.org')
-	println('OK!')
 
 	// Test MX record
-	print('Testing MX record...')
 	result = vdns.query(vdns.Query{ domain: 'fleximus.org', @type: vdns.Type.mx, resolver: resolver }) !
 	assert(result.answers[0].record == '10 bouncer.mxsystem.de')
-	println('OK!')
 
 	// Test TXT record
-	print('Testing TXT record...')
 	result = vdns.query(vdns.Query{ domain: 'fleximus.org', @type: vdns.Type.txt, resolver: resolver }) !
 	assert(result.answers[0].record == 'v=spf1 mx -all')
-	println('OK!')
-
-	println("*** ALL TESTS PASSED ***")
 }
